86d889f8d35c9ae1e483ed78ca9a50e34e1688d2
dominik <dominik.schiller@informatik.uni-augsburg.de>
2021-05-18 16:17:44 -0700

    Update hcai_ckplus to prioritize unique subjects in sets over equal label distribution

--- hcai_datasets/hcai_ckplus/hcai_ckplus.py
+++ hcai_datasets/hcai_ckplus/hcai_ckplus.py
@@ -3,6 +3,7 @@
 import os
 import glob
 import tensorflow_datasets as tfds
+import random
 
 # TODO(hcai_ckplus): Markdown description  that will appear on the catalog page.
 _DESCRIPTION = """
@@ -53,23 +54,26 @@ class HcaiCkplus(tfds.core.GeneratorBasedBuilder):
   def _standard_splits(self):
     """Returns the standard splits for the dataset.
     Since clplus has no predefined splits the function applies a 70-15-15 split ratio to create them.
-    Splits are sorted by filename and contain an equal emotion distribution."""
+    Splits are sorted by filename and contain only unique subjects."""
 
     emotion_files = glob.glob(os.path.join(self.dataset_dir, 'Emotion', '**/**/*.txt'))
-    samples_classwise = {l: [] for l in self.labels}
+    samples = []
 
     for ef in emotion_files:
       with open(ef, 'r') as f:
         emotion = self.labels[int(float(f.readline().strip()))]
-        fn = ef.split(os.sep)[-1]
-        samples_classwise[emotion].append(fn)
+        fn = ef.split(os.sep)[-1].replace('_emotion.txt', '.png')
+        samples.append((fn, emotion))
 
-    train, val, test = [], [], []
+    # since we do not have any specific neutral images directly labeled we use the first image of every sequence
+    samples.extend([(x.split(os.sep)[-1], 'neutral') for x in sorted(glob.glob(os.path.join(self.dataset_dir, 'cohn-kanade-images', '**/**/*01.png')))])
 
-    for k,v in samples_classwise.items():
-      train.extend([ (x, k) for x in v[:int(len(v) * 0.7)]])
-      val.extend([ (x, k) for x in v[int(len(v) * 0.7):int(len(v) * 0.85)]])
-      test.extend([ (x, k) for x in v[int(len(v) * 0.85):]])
+    unique_subjects = list(set([x[0].split('_')[0] for x in samples]))
+    random.Random(1337).shuffle(unique_subjects)
+
+    train = [s for u in unique_subjects[:int(len(unique_subjects) * 0.7)] for s in samples if s[0].startswith(u)]
+    val = [s for u in unique_subjects[int(len(unique_subjects) * 0.7):int(len(unique_subjects) * 0.85)] for s in samples if s[0].startswith(u)]
+    test = [s for u in unique_subjects[int(len(unique_subjects) * 0.85):] for s in samples if s[0].startswith(u)]
 
     return train, val, test
 
@@ -88,6 +92,6 @@ class HcaiCkplus(tfds.core.GeneratorBasedBuilder):
 
     for f, e in files:
       yield f, {
-        'image': os.path.join(self.dataset_dir, 'cohn-kanade-images', *f.split('_')[:-2], f.replace('_emotion.txt', '.png')),
+        'image': os.path.join(self.dataset_dir, 'cohn-kanade-images', *f.split('_')[:-1], f),
         'label': e,
       }
