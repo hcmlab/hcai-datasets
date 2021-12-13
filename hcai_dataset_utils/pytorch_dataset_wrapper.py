bd1c37c8e1660296509f10c4ba257d91004b3ae2
Valentin Kroner <valentin@GPU-VM.informatik.uni-augsburg.de>
2021-12-13 12:00:28 +0100

    optimized dataset wrapper

--- hcai_dataset_utils/pytorch_dataset_wrapper.py
+++ hcai_dataset_utils/pytorch_dataset_wrapper.py
@@ -1,89 +1,78 @@
-from tensorflow.python.data import Dataset
 from torch.utils.data import Dataset as TorchDataset
 from torch.utils.data.dataset import T_co
-import torch
 import tensorflow as tf
-import numpy as np
 
 
 class PyTorchDatasetWrapper(TorchDataset):
-    def __init__(
-        self,
-        dataset: Dataset,
-        load_to_device: bool = False,
-    ):
+    """
+    Adapts an non-batched, finite tensorflow dataset into a pytorch dataset for use with a pytorch dataloader.
+    This class can process both dict and tuple output datasets.
+    For performance reasons, the wrapper will batch the set internally,
+    but will still return single data points as numpy arrays for the dataloader.
+    """
+
+    def __init__(self, dataset: tf.data.Dataset, internal_cache_size: int = 32):
+        """
+        :param dataset: any unbatched, finite tensorflow dataset
+        :param internal_cache_size: the size of the internal batch cache
+        """
         self._original_ds_reference = dataset
-        self._iter = None
         self._output_dict = False
+        self._iter = None
+        self._batch_cache = None
+        self._batch_cache_size = internal_cache_size
+        self._internal_cache_size = 32
+        self._configure()
 
-        if torch.cuda.is_available():
-            self._device = torch.device("cuda")
-        else:
-            self._device = torch.device("cpu")
+    def _configure(self):
 
-        if load_to_device:
-            self._load_to_device()
-        else:
-            self._tensor_representation = None
-            self._len = tf.data.experimental.cardinality(dataset).numpy()
-            if self._len < 0:
-                self._find_length()
-
-    def _load_to_device(self):
-
-        self._len = 0
-        # process dict type sets
+        # is the set dict or tuple based?
         if isinstance(self._original_ds_reference.element_spec, dict):
             self._output_dict = True
-            self._tensor_representation = {}
-            for k in self._original_ds_reference.element_spec.keys():
-                if self._original_ds_reference.element_spec[k].dtype != tf.string:
-                    self._tensor_representation[k] = []
 
-            for data in self._original_ds_reference:
-                for k in self._tensor_representation.keys():
-                    as_tensor = torch.tensor(data[k].numpy()).to(self._device)
-                    self._tensor_representation[k].append(as_tensor)
-                self._len = self._len + 1
-        # process tuple sets, typically x,y for classifiers
-        else:
-            self._tensor_representation = None
-            for data in self._original_ds_reference:
-                if self._tensor_representation is None:
-                    self._tensor_representation = [[] for i in data]
-                for i, field in enumerate(data):
-                    as_tensor = torch.tensor(field.numpy()).to(self._device)
-                    self._tensor_representation[i].append(as_tensor)
-                self._len = self._len + 1
+        self._original_ds_reference = self._original_ds_reference.batch(
+            batch_size=self._internal_cache_size
+        )
 
-    def _find_length(self):
+        # iteratively find total item count, since tf.cardinality is unreliably in this context
         self._len = 0
-        for i in self._original_ds_reference:
-            self._len = self._len + 1
+        self._first_member = 0
+        for i, b in enumerate(self._original_ds_reference):
+            if self._output_dict and i == 0:
+                self._first_member = list(b.keys())[0]
+            self._len = self._len + tf.shape(b[self._first_member])[0].numpy()
 
     def __getitem__(self, index) -> T_co:
-        # preprocessed as gpu tensor
-        if self._tensor_representation is not None:
-            if self._output_dict:
-                r = {}
-                for k in self._tensor_representation.keys():
-                    r[k] = self._tensor_representation[k][index]
-                return r
-            else:
-                return [i[index] for i in self._tensor_representation]
 
-        # return dynamically from tf pipeline
+        # on epoch start (or error), create tf iterator
         if index == 0 or self._iter is None:
             self._iter = iter(self._original_ds_reference)
-        data = self._iter.get_next()
 
+        # fetch a new batch if current batch is exhausted
+        if (
+            index % self._internal_cache_size == 0
+            or index % self._batch_cache_size == 0
+        ):
+            if self._output_dict:
+                batch = self._iter.get_next()
+                self._batch_cache = {k: batch[k].numpy() for k in batch.keys()}
+                self._batch_cache_size = tf.shape(
+                    self._batch_cache[self._first_member]
+                )[0].numpy()
+            else:
+                self._batch_cache = [field.numpy() for field in self._iter.get_next()]
+                self._batch_cache_size = tf.shape(self._batch_cache[0])[0].numpy()
+
+        # return as dict or list of numpy arrays
         if self._output_dict:
-            r = {}
-            for k in data.keys():
-                # todo catch string types
-                r[k] = torch.tensor(data[index]).to(self._device)
+            return {
+                k: self._batch_cache[k][index % self._batch_cache_size]
+                for k in self._batch_cache.keys()
+            }
         else:
-            return [torch.tensor(field.numpy()).to(self._device) for field in data]
+            return [
+                field[index % self._batch_cache_size] for field in self._batch_cache
+            ]
 
     def __len__(self):
         return self._len
